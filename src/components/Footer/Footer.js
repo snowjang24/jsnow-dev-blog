194903e312b5bf22df90e1cadc92c3d1a62729aa
snowjang24 <asdka4618@gmail.com>
2020-03-25 02:32:51 +0900

    fix: git merge error fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
+import * as Styled from "./styled";
+import { FaFacebookSquare, FaGithubSquare, FaInstagram } from "react-icons/fa";
 
 export default ({ className }) => {
   const contactList = [
@@ -7,7 +9,6 @@ export default ({ className }) => {
     { url: "https://github.com/snowjang24", icon: FaGithubSquare },
     { url: "https://instagram.com/snow_jang24", icon: FaInstagram }
   ];
-
   const data = useStaticQuery(graphql`
     query {
       site {
